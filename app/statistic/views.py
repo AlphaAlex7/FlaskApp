ddef548f21a90cce7a718058e28b14af684052e3
AASPEE <alphaalex7@yandex.ru>
2022-05-05 17:39:34 +0300

    refactoring subscribe views from using macros in template

--- app/statistic/views.py
+++ app/statistic/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import current_user
 from . import statistic
+from ..servises import get_channels_for_user, get_date_for_filter
 
 
 @statistic.route("/")
@@ -10,7 +11,18 @@ def start():
 
 @statistic.route("/subscribe/")
 def subscribe():
-    return render_template("dashboard/statistic_subscribe.html", path_url="subscribe")
+    channels = get_channels_for_user(current_user)
+    option_channel = [
+        {"value": channel.id, "name": channel.name}
+        for channel in channels
+    ]
+    option_date = get_date_for_filter()
+    return render_template(
+        "dashboard/statistic_subscribe.html",
+        path_url="subscribe",
+        option_channel=option_channel,
+        option_date=option_date
+    )
 
 
 @statistic.route("/content/")
