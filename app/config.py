5b6e080e094c217e9b789432c393a2a38bce0b0f
AASPEE <alphaalex7@yandex.ru>
2022-08-18 17:32:50 +0300

    edit config

--- app/config.py
+++ app/config.py
@@ -12,12 +12,23 @@ class Config:
 
 class LocalConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'data.sqlite')}"
+    # SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'data.sqlite')}"
+    SQLALCHEMY_DATABASE_URI = ''.join(
+        f"postgresql://postgres"
+        f":postgres"
+        f"@localhost:5432/flaskapp"
+    )
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "strVerySecret"
-    CELERY_BROKER_URL = 'redis://redis:6379/0'
-    result_backend = 'redis://redis:6379/0'
+    # CELERY_BROKER_URL = 'redis://redis:6379/0'
+    # CELERY_BROKER = 'redis://redis:6379/0'
+    # RESULT_BACKEND = 'redis://redis:6379/0'
+    CELERY_BROKER_URL = 'redis://default:redispw@localhost:49153'
+    CELERY_BROKER = 'redis://default:redispw@localhost:49154'
+    RESULT_BACKEND = 'redis://default:redispw@localhost:49154'
+    CELERY_TIMEZONE = 'Europe/Moscow'
+    CELERY_IMPORTS = ("app.celery_app.tasks",)
 
 
 class TestingConfig(LocalConfig):
@@ -29,8 +40,13 @@ class TestingConfig(LocalConfig):
 
 class DockerConfig(LocalConfig):
     DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = ''.join(
+    #     f"postgresql://postgres"
+    #     f":postgres"
+    #     f"@flaskpg-13.3:5432/flaskapp"
+    # )
     SQLALCHEMY_DATABASE_URI = ''.join(
         f"postgresql://{os.environ.get('POSTGRES_USER')}"
-        f":{os.environ.get('AlphaPass')}"
-        f"@db:5432/{os.environ.get('flaskapp')}"
+        f":{os.environ.get('POSTGRES_PASSWORD')}"
+        f"@db:5432/{os.environ.get('POSTGRES_DB')}"
     )
